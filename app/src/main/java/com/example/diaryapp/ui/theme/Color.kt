a81392d696dec69658e95667b8c93d33d1032e35
saimanchen <saiman.chen@outlook.com>
2023-09-05 15:02:07 +0200

    Initial commit

---
+++ app/src/main/java/com/example/diaryapp/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+package com.example.diaryapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// light color theme
+val SchoolBus = Color(0xFFFFD803)
+val SpacePurple = Color(0xFF272343)
+val MintGreen = Color(0xFFBAE8E8)
+val LightBackground = Color(0xFFF3FBFB)
+
+// dark color theme
+val Orange = Color(0xFFFE8811)
+val Mint = Color(0xFF1EC9A6)
+val White = Color(0xFFFFFFFF)
+val DarkBackground = Color(0xFF350B06)
