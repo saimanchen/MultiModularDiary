e4b685e8917b30823aa81ccb555cf0d9566b0713
saimanchen <saiman.chen@outlook.com>
2023-09-07 14:32:27 +0200

    add client id for google authentication

--- app/src/main/java/com/example/diaryapp/util/Constants.kt
+++ app/src/main/java/com/example/diaryapp/util/Constants.kt
@@ -2,4 +2,5 @@ package com.example.diaryapp.util
 
 object Constants {
     const val APP_ID = "mydiaryapp-wqhzg"
+    const val CLIENT_ID = "524189929867-eec68oel94ph5gjf7q4e147t2kfemd56.apps.googleusercontent.com"
 }
