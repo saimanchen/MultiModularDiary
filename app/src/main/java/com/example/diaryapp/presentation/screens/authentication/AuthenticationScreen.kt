23bfcb1c21a13ddea18247ce716b9c7be85601ab
saimanchen <saiman.chen@outlook.com>
2023-09-08 12:39:20 +0200

    add google button

---
+++ app/src/main/java/com/example/diaryapp/presentation/screens/authentication/AuthenticationScreen.kt
@@ -0,0 +1,13 @@
+package com.example.diaryapp.presentation.screens.authentication
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AuthenticationScreen() {
+    
+}
+
+@Composable
+fun AuthenticationContent() {
+    
+}
