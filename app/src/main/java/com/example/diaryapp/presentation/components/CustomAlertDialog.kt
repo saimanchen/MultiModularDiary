671be0490ef9322bac0f8b04692ca1ea30bc48e8
saimanchen <saiman.chen@outlook.com>
2023-09-12 20:41:39 +0200

    add mood icons, add model Mood & Diary

--- app/src/main/java/com/example/diaryapp/presentation/components/CustomAlertDialog.kt
+++ app/src/main/java/com/example/diaryapp/presentation/components/CustomAlertDialog.kt
@@ -1,23 +1,18 @@
 package com.example.diaryapp.presentation.components
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
