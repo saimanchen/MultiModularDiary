d9d98817998a041efbd932740b6f7287f82c7997
saimanchen <saiman.chen@outlook.com>
2023-09-18 20:34:41 +0200

    add text fields for entry title and description

--- app/src/main/java/com/example/diaryapp/presentation/screens/write/WriteScreen.kt
+++ app/src/main/java/com/example/diaryapp/presentation/screens/write/WriteScreen.kt
@@ -1,8 +1,11 @@
 package com.example.diaryapp.presentation.screens.write
 
 import android.annotation.SuppressLint
+import android.widget.Space
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -10,16 +13,26 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -27,9 +40,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import com.example.diaryapp.R
 import com.example.diaryapp.model.Diary
@@ -53,13 +68,24 @@ fun WriteScreen(
             )
         },
         content = {
-            WriteContent(paddingValues = it)
+            WriteContent(
+                title = "",
+                onTitleChanged = {},
+                description = "",
+                onDescriptionChanged = {},
+                paddingValues = it
+            )
         }
     )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun WriteContent(
+    title: String,
+    onTitleChanged: (String) -> Unit,
+    description: String,
+    onDescriptionChanged: (String) -> Unit,
     paddingValues: PaddingValues
 ) {
     val scrollState = rememberScrollState()
@@ -95,6 +121,84 @@ fun WriteContent(
                 ChangeMoodIconAction()
             }
             Spacer(modifier = Modifier.height(24.dp))
+            BoxWithConstraints {
+                var max = maxWidth
+                Column {
+                    TextField(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .requiredWidth(max + 16.dp)
+                            .offset(x = (-8).dp),
+                        value = title,
+                        onValueChange = onTitleChanged,
+                        placeholder = {
+                            Text(text = "Title")
+                        },
+                        colors = TextFieldDefaults.textFieldColors(
+                            containerColor = Color.Transparent,
+                            focusedIndicatorColor = Color.Unspecified,
+                            disabledIndicatorColor = Color.Unspecified,
+                            unfocusedIndicatorColor = Color.Unspecified,
+                            placeholderColor = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.5f)
+                        ),
+                        keyboardOptions = KeyboardOptions(
+                            imeAction = ImeAction.Next
+                        ),
+                        keyboardActions = KeyboardActions(
+                            onNext = {}
+                        ),
+                        maxLines = 1,
+                        singleLine = true
+                    )
+                    Spacer(modifier = Modifier.height(24.dp))
+                    TextField(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .requiredWidth(max + 16.dp)
+                            .offset(x = (-8).dp),
+                        value = title,
+                        onValueChange = onTitleChanged,
+                        placeholder = {
+                            Text(text = "Tell me about it!")
+                        },
+                        colors = TextFieldDefaults.textFieldColors(
+                            containerColor = Color.Transparent,
+                            focusedIndicatorColor = Color.Unspecified,
+                            disabledIndicatorColor = Color.Unspecified,
+                            unfocusedIndicatorColor = Color.Unspecified,
+                            placeholderColor = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.5f)
+                        ),
+                        keyboardOptions = KeyboardOptions(
+                            imeAction = ImeAction.Next
+                        ),
+                        keyboardActions = KeyboardActions(
+                            onNext = {}
+                        )
+                    )
+                }
+            }
+        }
+
+        Column(verticalArrangement = Arrangement.Bottom) {
+            Spacer(modifier = Modifier.height(12.dp))
+            OutlinedButton(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = { /*TODO*/ },
+                shape = Shapes().extraSmall,
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Transparent,
+                    contentColor = MaterialTheme.colorScheme.tertiary
+                ),
+                border = BorderStroke(width = 1.dp, color = MaterialTheme.colorScheme.tertiary)
+            ) {
+                Text(
+                    text = "Save",
+                    style = TextStyle(
+                        fontSize = MaterialTheme.typography.titleMedium.fontSize,
+                        fontWeight = FontWeight.Light
+                    )
+                )
+            }
         }
     }
 }
