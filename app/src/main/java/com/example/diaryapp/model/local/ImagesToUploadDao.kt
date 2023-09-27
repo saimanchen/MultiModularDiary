7c065ce4f33ef8975eea2fe8f687660ca494fccc
saimanchen <saiman.chen@outlook.com>
2023-09-27 13:53:46 +0200

    Update ImagesToUploadDao.kt

--- app/src/main/java/com/example/diaryapp/model/local/ImagesToUploadDao.kt
+++ app/src/main/java/com/example/diaryapp/model/local/ImagesToUploadDao.kt
@@ -8,12 +8,12 @@ import com.example.diaryapp.model.local.entity.ImageToUpload
 
 @Dao
 interface ImagesToUploadDao {
-    @Query("SELECT * FROM images_to_upload_table ORDER BY id ASC")
+    @Query("SELECT * FROM image_to_upload_table ORDER BY id ASC")
     suspend fun getAllImages(): List<ImageToUpload>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun addImageToUpload(imageToUpload: ImageToUpload)
 
-    @Query("DELETE FROM images_to_upload_table WHERE id=:imageId")
+    @Query("DELETE FROM image_to_upload_table WHERE id=:imageId")
     suspend fun cleanupImage(imageId: Int)
 }
