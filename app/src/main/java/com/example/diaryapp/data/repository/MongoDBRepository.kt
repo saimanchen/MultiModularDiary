c2c7dcf5bc60cb2ec7d435294dfec97d69a12da1
saimanchen <saiman.chen@outlook.com>
2023-10-06 12:30:33 +0200

    add function to MongoDB & MongoDBRepository

--- app/src/main/java/com/example/diaryapp/data/repository/MongoDBRepository.kt
+++ app/src/main/java/com/example/diaryapp/data/repository/MongoDBRepository.kt
@@ -5,12 +5,14 @@ import com.example.diaryapp.util.RequestState
 import kotlinx.coroutines.flow.Flow
 import org.mongodb.kbson.ObjectId
 import java.time.LocalDate
+import java.time.ZonedDateTime
 
 typealias DiaryEntries = RequestState<Map<LocalDate, List<Diary>>>
 
 interface MongoDBRepository {
     fun configureRealm()
     fun getAllDiaryEntries(): Flow<DiaryEntries>
+    fun getFilteredDiaryEntries(zonedDateTime: ZonedDateTime): Flow<DiaryEntries>
     fun getSelectedDiaryEntry(diaryId: ObjectId): Flow<RequestState<Diary>>
     suspend fun insertDiaryEntry(diary: Diary): RequestState<Diary>
     suspend fun updateDiaryEntry(diary: Diary): RequestState<Diary>
