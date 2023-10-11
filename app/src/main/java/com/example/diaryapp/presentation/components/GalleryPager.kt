f5421eeafe28b4861260f86a31d07dfae3548ee2
saimanchen <saiman.chen@outlook.com>
2023-10-11 13:49:29 +0200

    add PagerNavigation composable

--- app/src/main/java/com/example/diaryapp/presentation/components/GalleryPager.kt
+++ app/src/main/java/com/example/diaryapp/presentation/components/GalleryPager.kt
@@ -1,18 +1,33 @@
 package com.example.diaryapp.presentation.components
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
+import com.example.diaryapp.R
 import com.example.diaryapp.presentation.screens.write.DiaryState
 import com.example.diaryapp.util.GalleryState
 
@@ -24,21 +39,72 @@ fun GalleryPager(
     Surface(
         modifier = Modifier.fillMaxSize()
     ) {
-        val pagerState = rememberPagerState(initialPage = 0)
         val context = LocalContext.current
+        val pagerState = rememberPagerState(initialPage = 0)
 
-        HorizontalPager(
-            pageCount = galleryState.images.size,
-            state = pagerState
-        ) { page ->
-            AsyncImage(
-                modifier = Modifier.fillMaxSize(),
-                model = ImageRequest.Builder(context)
-                    .data(galleryState.images[page].image)
-                    .crossfade(true)
-                    .build(),
-                contentDescription = null,
-                contentScale = ContentScale.FillWidth
+        Column {
+            HorizontalPager(
+                modifier = Modifier.weight(1f),
+                pageCount = galleryState.images.size,
+                state = pagerState
+            ) { page ->
+                AsyncImage(
+                    modifier = Modifier.fillMaxSize(),
+                    model = ImageRequest.Builder(context)
+                        .data(galleryState.images[page].image)
+                        .crossfade(true)
+                        .build(),
+                    contentDescription = null,
+                    contentScale = ContentScale.FillWidth
+                )
+            }
+            PagerNavigation()
+        }
+    }
+}
+
+@Composable
+fun PagerNavigation() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 24.dp),
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+        IconButton(
+            onClick = { /*TODO*/ },
+            colors = IconButtonDefaults.iconButtonColors(contentColor = MaterialTheme.colorScheme.primary)
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.baseline_keyboard_double_arrow_left_24),
+                contentDescription = "Navigate to first image"
+            )
+        }
+        IconButton(
+            onClick = { /*TODO*/ },
+            colors = IconButtonDefaults.iconButtonColors(contentColor = MaterialTheme.colorScheme.primary)
+        ) {
+            Icon(
+                imageVector = Icons.Default.KeyboardArrowLeft,
+                contentDescription = "Navigate to previous image"
+            )
+        }
+        IconButton(
+            onClick = { /*TODO*/ },
+            colors = IconButtonDefaults.iconButtonColors(contentColor = MaterialTheme.colorScheme.primary)
+        ) {
+            Icon(
+                imageVector = Icons.Default.KeyboardArrowRight,
+                contentDescription = "Navigate to next image"
+            )
+        }
+        IconButton(
+            onClick = { /*TODO*/ },
+            colors = IconButtonDefaults.iconButtonColors(contentColor = MaterialTheme.colorScheme.primary)
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.baseline_keyboard_double_arrow_right_24),
+                contentDescription = "Navigate to last image"
             )
         }
     }
