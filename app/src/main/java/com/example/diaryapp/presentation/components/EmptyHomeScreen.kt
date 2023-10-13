9029c75d9474b710023c6522c25f9a41fa4084f5
saimanchen <saiman.chen@outlook.com>
2023-10-13 11:19:06 +0200

    Add stringResources to EmptyHomeScreen

--- app/src/main/java/com/example/diaryapp/presentation/components/EmptyHomeScreen.kt
+++ app/src/main/java/com/example/diaryapp/presentation/components/EmptyHomeScreen.kt
@@ -10,17 +10,19 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import com.example.diaryapp.R
 import com.example.diaryapp.model.remote.Diary
 import java.time.LocalDate
 
 @Composable
 fun EmptyHomeScreen(
     diaryEntries: Map<LocalDate, List<Diary>> = mapOf(),
-    title: String = "Empty Diary",
-    subtitle: String = "Write your first diary note",
+    title: String = stringResource(id = R.string.empty_diary),
+    subtitle: String = stringResource(id = R.string.write_your_first_diary_note),
     isDateSelected: Boolean
 ) {
     Column(
@@ -33,7 +35,7 @@ fun EmptyHomeScreen(
     ) {
         Text(
             text = if (isDateSelected && diaryEntries.isEmpty()) {
-                "Nothing was written this day"
+                stringResource(id = R.string.nothing_was_written)
             } else {
                 title
             },
@@ -46,7 +48,7 @@ fun EmptyHomeScreen(
         )
         Text(
             text = if (isDateSelected && diaryEntries.isEmpty()) {
-                "Choose another date"
+                stringResource(id = R.string.choose_another_day)
             } else {
                 subtitle
             },
