56dc6f76b4cca2494be48a604b80bb52ae054df0
saimanchen <saiman.chen@outlook.com>
2023-10-23 13:14:21 +0200

    Fix logic for filter query

--- app/src/main/java/com/example/diaryapp/data/repository/MongoDB.kt
+++ app/src/main/java/com/example/diaryapp/data/repository/MongoDB.kt
@@ -14,6 +14,8 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
 import org.mongodb.kbson.ObjectId
+import java.time.LocalDateTime
+import java.time.LocalTime
 import java.time.ZoneId
 import java.time.ZonedDateTime
 
@@ -69,8 +71,18 @@ object MongoDB : MongoDBRepository {
                 realm.query<Diary>(
                     "ownerId == $0 AND date < $1 AND date > $2",
                     user.id,
-                    RealmInstant.from(zonedDateTime.plusDays(1).toInstant().epochSecond, 0),
-                    RealmInstant.from(zonedDateTime.minusDays(1).toInstant().epochSecond, 0)
+                    RealmInstant.from(
+                        LocalDateTime.of(
+                            zonedDateTime.toLocalDate().plusDays(1),
+                            LocalTime.MIDNIGHT
+                        ).toEpochSecond(zonedDateTime.offset), 0
+                    ),
+                    RealmInstant.from(
+                        LocalDateTime.of(
+                            zonedDateTime.toLocalDate(),
+                            LocalTime.MIDNIGHT
+                        ).toEpochSecond(zonedDateTime.offset), 0
+                    )
                 ).asFlow().map { result ->
                     RequestState.Success(
                         data = result.list.groupBy {
